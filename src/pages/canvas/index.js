67c6e19845416ccfd428e228c056214c4a8f49da
Kim Seok Yoon <kjkksu2@naver.com>
2023-05-10 14:58:14 +0900

    canvas

--- src/pages/canvas/index.js
+++ src/pages/canvas/index.js
@@ -1,15 +1,5 @@
 // 여기서 동적 import
 import "./styles.css";
 
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
-
-canvas.width = 900;
-canvas.height = 500;
-
-window.onload = () => {
-  ctx.beginPath();
-  ctx.moveTo(100, 100);
-  ctx.lineTo(200, 200);
-  ctx.stroke();
-};
+import "./polygon";
+// import "./momentum";
