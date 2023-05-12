d11d1c37def0cdc1a07dc2ffe6c2b30a26dff133
Kim Seok Yoon <kjkksu2@naver.com>
2023-05-12 19:18:36 +0900

    polygon

--- src/pages/canvas/index.js
+++ src/pages/canvas/index.js
@@ -1,6 +1,6 @@
 // 여기서 동적 import
 import "./styles.css";
 
-import "./chart";
+// import "./chart";
 // import "./breakOut";
-// import "./polygon";
+import "./polygon";
---
+++ src/pages/canvas/polygon/class/configuration.js
@@ -0,0 +1,50 @@
+class Configuration {
+  radian = Math.PI / 180;
+
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext("2d");
+
+    this.canvas.width = 1000;
+    this.canvas.height = 700;
+
+    this.init();
+    this.style();
+  }
+
+  init() {
+    this.sides = 5;
+    this.angle = 360 / this.sides;
+    this.centerX = this.canvas.width / 2;
+    this.centerY = this.canvas.height / 2;
+    this.radius = 270;
+  }
+
+  style() {
+    this.axisStyle();
+    this.circleStyle();
+    this.pointStyle();
+    this.polygonStyle();
+  }
+
+  axisStyle() {
+    this.axisWidth = 1;
+    this.axisColor = "rgba(0, 0, 0, 0.3)";
+  }
+
+  circleStyle() {
+    this.circleWidth = 3;
+    this.circleColor = "rgba(0, 0, 0, 0.3)";
+  }
+
+  pointStyle() {
+    this.pointColor = "red";
+  }
+
+  polygonStyle() {
+    this.polygonWidth = 3;
+    this.polygonColor = "black";
+  }
+}
+
+export default Configuration;
