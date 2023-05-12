e95244c9dea41eb0426d466f728cae4f708561ad
Kim Seok Yoon <kjkksu2@naver.com>
2023-05-12 18:28:10 +0900

    chart

--- src/pages/canvas/chart/class/configuration.js
+++ src/pages/canvas/chart/class/configuration.js
@@ -6,6 +6,10 @@ class Configuration {
   constructor(canvas) {
     this.canvas = canvas;
     this.ctx = this.canvas.getContext("2d");
+
+    this.canvas.width = 1000;
+    this.canvas.height = 700;
+
     this.refineData();
     this.init();
   }
--- src/pages/canvas/chart/index.js
+++ src/pages/canvas/chart/index.js
@@ -1,9 +1,5 @@
 import Chart from "./class/chart";
 
 const canvas = document.querySelector("canvas");
-
-canvas.width = 1000;
-canvas.height = 700;
-
 const chart = new Chart(canvas);
 chart.draw();
