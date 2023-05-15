b21a72fb9a18fae0d0122a58871a700ad2ca868f
Kim Seok Yoon <kjkksu2@naver.com>
2023-05-15 19:12:59 +0900

    momentum

---
+++ src/pages/canvas/momentum/class/helper.js
@@ -0,0 +1,9 @@
+class Helper {
+  static getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+}
+
+export default Helper;
