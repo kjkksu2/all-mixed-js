92381881a7a236a09d2430106bec5b60ee0ddce9
Kim Seok Yoon <kjkksu2@naver.com>
2023-06-05 20:56:08 +0900

    output.js

--- src/pages/myArray/components/output.js
+++ src/pages/myArray/components/output.js
@@ -1,4 +1,6 @@
-class Ouput {
+import DomElements from "./domElements";
+
+class Output extends DomElements {
   /*
     eval이 평가하는 코드는 const output = document.querySelector(".output")이다.
     따라서, this.output이 아니라 output으로 적어야 한다.
@@ -6,43 +8,64 @@ class Ouput {
   */
   print(value) {
     if (value === undefined) {
-      output.innerHTML += "undefined" + "\n";
+      this.output.innerHTML += "undefined" + "\n";
       return;
     }
 
     if (value === null) {
-      output.innerHTML += "null" + "\n";
+      this.output.innerHTML += "null" + "\n";
       return;
     }
 
     // [1,2,3]으로 출력되는 것을 [1, 2, 3]으로 바꿈. (filter 등)
     if (Object.getPrototypeOf(value) === Array.prototype) {
       const array = value;
-      output.innerHTML += JSON.stringify(array).replaceAll(",", ", ") + "\n";
+      this.output.innerHTML +=
+        JSON.stringify(array).replaceAll(",", ", ") + "\n";
       return;
     }
 
     // [1,2,3]으로 출력되는 것을 [1, 2, 3]으로 바꿈. (filter 등)
     if (Object.getPrototypeOf(value) === MyArray.prototype) {
       const array = value.values;
-      output.innerHTML += JSON.stringify(array).replaceAll(",", ", ") + "\n";
+      this.output.innerHTML +=
+        JSON.stringify(array).replaceAll(",", ", ") + "\n";
       return;
     }
 
     // 1,2,3으로 출력되는 것을 "1,2,3"으로 바꿈. (join)
     if (Object.getPrototypeOf(value) === String.prototype) {
-      output.innerHTML += JSON.stringify(value) + "\n";
+      this.output.innerHTML += JSON.stringify(value) + "\n";
       return;
     }
 
-    output.innerHTML += value + "\n";
+    this.output.innerHTML += value + "\n";
   }
 
-  stringify() {
-    const overridedConsole = Object.getPrototypeOf(this).print;
+  connection() {
+    function Output() {
+      DomElements.call(this);
+    }
 
-    return `console.log = ${overridedConsole};`;
+    Output.prototype = Object.create(DomElements.prototype);
+    DomElements.prototype.constructor = DomElements;
+  }
+
+  /* 
+    es5이하에서도 동작해야 한다.
+    extends 키워드 말고 직접 연결하자.
+  */
+  stringify() {
+    let str = "";
+
+    str += this.connection;
+
+    console.log(str);
+
+    str += `const a = ${(value) => this.print(value)};`;
+
+    return str;
   }
 }
 
-export default Ouput;
+export default Output;
